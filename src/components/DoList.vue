73065546e8506abaa97cb1ffa2b10e434d7806e5
Serhat Serter <serhatserter02@outlook.com>
2019-07-18 09:58:18 +0300

    last

--- src/components/DoList.vue
+++ src/components/DoList.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="dolist" class="text-center">
     <br />
+
     <h1 class="sitetitle">To Do List</h1>
 
     <div id="adding">
-      <input id="new" type="textbox" v-model="inputed" @keyup.enter="addList(inputed)" />
+      <input id="new" type="textbox" :maxlength="maxchar" v-model="inputed" @keyup.enter="addList(inputed)" />
       <b-button variant="success" id="add" @click="addList(inputed)">+</b-button>
     </div>
 
     <div id="changing">
+      <div id="charactercount">Remaining Characters: {{remainingChar}}</div>
+      <br>
       <label v-for="(tab, i) in tabs" :key="i">
         <div id="radioButtons">
           <input type="radio" name="tab" :checked="tab.status" @input="changeTab(i)" />
@@ -19,6 +22,10 @@
 
       <br />
       <br />
+
+    <b-alert id="alert" v-model="showDismissibleAlert" variant="danger" dismissible>
+      {{alertmessage}}
+    </b-alert>
 
       <h4 class="sitetitle">List:</h4>
     </div>
@@ -43,226 +50,270 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      inputed: "",
+  export default {
+    data() {
+      return {
+        inputed: "",
+        showDismissibleAlert: false,
+        alertmessage: "",
+        maxchar: 10,
 
-      doList: [
-        {
-          id: this.randomKey(),
-          title: "Example Task",
-          status: "active" //completed
-        }
-      ],
-      tabs: [
-        {
-          title: "All",
-          status: true,
-          count: 0
-        },
-        {
-          title: "Active",
-          status: false,
-          count: 0
-        },
-        {
-          title: "Completed",
-          status: false,
-          count: 0
-        }
-      ],
-      selectList: []
-    };
-  },
-
-  props: {},
-
-  computed: {
-    currentTab() {
-      return this.tabs.find(tab => tab.status).title;
-    },
-
-    currentCounts() {
-      let list = this.doList;
-
-      switch (this.currentTab) {
-        case "Active":
-          list = this.doList.filter(todo => todo.status === "active");
-          return todo.length;
-          break;
-
-        case "Completed":
-          list = this.doList.filter(todo => todo.status === "completed");
-          break;
-      }
-    },
-
-    currentList() {
-      let list = this.doList;
-
-      switch (this.currentTab) {
-        case "Active":
-          list = this.doList.filter(todo => todo.status === "active");
-          break;
-
-        case "Completed":
-          list = this.doList.filter(todo => todo.status === "completed");
-          break;
-      }
-
-      return list;
-    }
-  },
-
-  methods: {
-    totalCount(val) {
-      if (val === "all") {
-        return this.doList.length;
-      }
-      return this.doList.filter(todo => todo.status === val).length;
-    },
-
-    randomKey() {
-      return Math.random()
-        .toString(16)
-        .slice(2);
-    },
-
-    addList(str) {
-      if (str !== null) {
-        str = str.trim();
-
-        if (
-          this.doList.filter(
-            v => v.title === str || v.title === " " || str.trim() === ""
-          ).length === 0
-        ) {
-          this.doList.push({
+        doList: [
+          {
             id: this.randomKey(),
-            title: str,
-            status: "active"
-          });
+            title: "Example Task",
+            status: "active" //completed
+          }
+        ],
+        tabs: [
+          {
+            title: "All",
+            status: true,
+            count: 0
+          },
+          {
+            title: "Active",
+            status: false,
+            count: 0
+          },
+          {
+            title: "Completed",
+            status: false,
+            count: 0
+          }
+        ],
+        selectList: []
+      };
+    },
+
+    watch: {
+        remainingChar: function(val){
+          if(this.remainingChar===0){
+            this.alertmessage = "Maximum Characters Reached.";
+            this.showDismissibleAlert = true;
+          }
+          else{
+            this.showDismissibleAlert=false;
+          }
         }
-        this.inputed = "";
+    },
+
+    computed: {
+      remainingChar: function(){
+        return this.maxchar - this.inputed.length;
+      },
+
+      currentTab() {
+        return this.tabs.find(tab => tab.status).title;
+      },
+
+      currentCounts() {
+        let list = this.doList;
+
+        switch (this.currentTab) {
+          case "Active":
+            list = this.doList.filter(todo => todo.status === "active");
+            return todo.length;
+            break;
+
+          case "Completed":
+            list = this.doList.filter(todo => todo.status === "completed");
+            break;
+        }
+      },
+
+      currentList() {
+        let list = this.doList;
+
+        switch (this.currentTab) {
+          case "Active":
+            list = this.doList.filter(todo => todo.status === "active");
+            break;
+
+          case "Completed":
+            list = this.doList.filter(todo => todo.status === "completed");
+            break;
+        }
+
+        return list;
       }
     },
 
-    delRow(num) {
-      this.doList.splice(num, 1);
-    },
+    methods: {
 
-    changeTab(tabIndex) {
-      this.tabs.forEach(
-        function(tab, i) {
-          this.tabs[i].status = false;
-        }.bind(this)
-      );
+      totalCount(val) {
+        if (val === "all") {
+          return this.doList.length;
+        }
+        return this.doList.filter(todo => todo.status === val).length;
+      },
 
-      this.tabs[tabIndex].status = true;
+      randomKey() {
+        return Math.random()
+          .toString(16)
+          .slice(2);
+      },
+
+      addList(str) {
+        if (str !== null) {
+          str = str.trim();
+
+          if (this.doList.filter(v => v.title === str || str.trim() === "").length === 0) {
+            
+            this.doList.push({
+              id: this.randomKey(),
+              title: str,
+              status: "active"
+
+            });
+            this.showDismissibleAlert= false;
+          }
+          else if (str.length!==0){
+            this.alertmessage = "Same Task Added!";
+            this.showDismissibleAlert = true;
+          }
+          else{
+            this.alertmessage = "Please, add text!";
+            this.showDismissibleAlert = true;
+          }
+
+          this.inputed = "";
+        }
+      },
+
+      delRow(num) {
+        this.doList.splice(num, 1);
+      },
+
+      changeTab(tabIndex) {
+        this.tabs.forEach(
+          function(tab, i) {
+            this.tabs[i].status = false;
+          }.bind(this)
+        );
+
+        this.tabs[tabIndex].status = true;
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
-#new {
-  float: inline-block;
-  width: 250px;
-  text-align: center;
-  margin-right: 10px;
-}
-.sitetitle {
-  color: rgb(148, 148, 148);
-}
 
-#adding {
-  padding: 20px;
-  padding-bottom: 0px;
-}
+  #new {
+    float: inline-block;
+    width: 38%;
+    text-align: center;
+    margin-right: 10px;
+  }
 
-#changing {
-  margin: 20px;
-}
+  .sitetitle{  
+    color:lightgray;
+  }
 
-#listing {
-  display: inline-block;
-  padding: 10px;
-  width: 50%;
-  height: 200px;
-}
-ul {
-  list-style: none;
-}
+   #charactercount {
+    color:lightgray;
+  }
 
-b-button {
-  width: 40px;
-  padding: 5px;
+  #adding {
+    padding: 20px;
+    padding-bottom: 0px;
+  }
 
-  text-align: center;
-  background-color: brown;
-  color: white;
-  float: inline-start;
-}
+  #changing {
+    margin: 20px;
+  }
 
-#add {
-  float: inline-start;
-}
+  #listing {
+    display: inline-block;
+    padding: 10px;
+    width: 50%;
+    height: 200px;
+  }
 
-input[type="checkbox"] {
-  display: none;
-}
-input[type="textbox"] {
-  padding: 5px;
-  border-style: solid;
-  border-width: 1px;
-  border-color: rgb(34, 34, 34);
-  border-radius: 10px;
-}
+  ul {
+    list-style: none;
+  }
 
-input[type="checkbox"] + label {
-  cursor: pointer;
-  float: inline-start;
-  width: 500px;
-  padding: 10px;
-  margin-left: -10px;
-  background-color: whitesmoke;
-}
-input[type="checkbox"]:checked + label {
-  background-color: darkgray;
-  text-decoration: line-through;
-}
+  b-button {
+    width: 40px;
+    padding: 5px;
 
-#radioButtons {
-  display: block;
-  padding: 10px;
-  background-color: whitesmoke;
-}
+    text-align: center;
+    background-color: brown;
+    color: white;
+    float: inline-start;
+  }
 
-#radioButtons:hover {
-  background-color: darkgray;
-}
+  #add {
+    float: inline-start;
+  }
 
-#radioButtons:checked {
-  background-color: darkgray;
-}
+  input[type="checkbox"] {
+    display: none;
+  }
 
-#radioButtonsCount {
-  background-color: darkgray;
-}
+  input[type="textbox"] {
+    padding: 5px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgb(34, 34, 34);
+    border-radius: 10px;
+  }
 
-#deletebutton{
-  font-size: 10px;
-}
-.todocontent {
-  border-style: solid;
-  border-width: 1px;
-  border-color: rgb(34, 34, 34);
-  border-radius: 10px;
-  margin-right: 10px;
-}
-.todocontent:hover {
-  background-color: brown;
-  color: white;
-}
+  input[type="checkbox"] + label {
+    cursor: pointer;
+    float: inline-start;
+    width: 85%;
+    padding: 10px;
+    margin-left: -10px;
+    background-color: whitesmoke;
+  }
+
+  input[type="checkbox"]:checked + label {
+    background-color: darkgray;
+    text-decoration: line-through;
+  }
+
+  #radioButtons {
+    display: block;
+    padding: 10px;
+    background-color: whitesmoke;
+  }
+
+  #radioButtons:hover {
+    background-color: darkgray;
+  }
+
+  #radioButtons:checked {
+    background-color: darkgray;
+  }
+
+  #radioButtonsCount {
+    background-color: darkgray;
+  }
+
+  #deletebutton {
+    font-size: 10px;
+  }
+
+  .todocontent {
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgb(34, 34, 34);
+    border-radius: 10px;
+    margin-right: 10px;
+  }
+
+  .todocontent:hover {
+    background-color: brown;
+    color: white;
+  }
+
+  #alert{
+    display: inline-block;
+    width: 40%;
+  }
+
+
 </style>
